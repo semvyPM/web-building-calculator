47402eb7e63a2816f19181ae13dd6360f21061a7
semvy <semvy@inbox.ru>
2024-02-19 19:50:19 +0400

    Added Vue components

---
+++ src/main.js
@@ -0,0 +1,8 @@
+// import './assets/style/index_page_style/responsive.css'
+// import './assets/style/index_page_style/style.css'
+
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from "@/components/router.js";
+createApp(App).use(router).mount('#app')
