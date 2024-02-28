23ca44ef94cbfa7aa1e1643487dc2e0a0318e8f2
serega12760 <serega12760@mail.ru>
2024-02-28 23:20:05 +0400

    Create ClientsPage.vue

---
+++ src/components/ClientsPage.vue
@@ -0,0 +1,82 @@
+
+<script setup>
+import Header from "@/components/Header.vue";
+</script>
+
+<template>
+  <Header/>
+  <div class="nav">
+    <div class="back"></div>
+    <div class="carcass">
+      <p>
+        Клиенты
+      </p>
+    </div>
+  </div>
+  <main>
+
+    <div class="adress">
+      <input type="button" value="Создать клиента" @click="togglePopup">
+    </div>
+      <CreateUserPopup v-if="showPopup" @close="showPopup = false"/>
+
+      <div class="card-container">
+        <div v-for="client in clients" :key="client.id">
+          <router-link :to="'/client/' + client.id"  v-if="client"><div class="card">{{ client.lastName }} {{ client.firstName }} {{ client.secondName }}</div></router-link>
+        </div>
+      </div>
+
+  </main>
+</template>
+
+<script>
+import CreateUserPopup from "@/components/CreateUserPopup.vue";
+import axios from "axios";
+
+export default {
+  components: {
+    CreateUserPopup
+  },
+  data() {
+    return {
+      showPopup: false,
+      clients: []
+    };
+  },
+  mounted() {
+    this.getClients()
+  },
+  methods: {
+    togglePopup() {
+      this.showPopup = !this.showPopup
+    },
+    saveAndRedirect() {
+      alert("saveAndRedirect");
+    },
+    async getClients() {
+      await axios.get('http://localhost:8080/api/clients/by-user/4')
+          .then(response => {
+            this.clients = response.data;
+            console.log(response.data);
+          })
+          .catch(error => {
+            console.error('Ошибка при получении списка клиентов', error);
+          });
+
+
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+@import '@/assets/style/carcas_page_style/style.css';
+@import '@/assets/style/carcas_page_style/slide.css';
+@import '@/assets/style/carcas_page_style/responsive.css';
+@import url(http://fonts.googleapis.com/css?family=Open+Sans:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i);
+@import url(http://fonts.googleapis.com/css?family=Open+Sans:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i);
+</style>
+
+
