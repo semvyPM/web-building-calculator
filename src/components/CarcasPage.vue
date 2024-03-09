e7e54ecf69400f8f2b44784bf79c1ac6613218aa
semvy <semvy@inbox.ru>
2024-03-10 00:10:57 +0400

    Added save calc method

--- src/components/CarcasPage.vue
+++ src/components/CarcasPage.vue
@@ -6,51 +6,53 @@ import Header from "@/components/Header.vue";
 </script>
 
 <template>
-<Header  :client="id" :clientData="true"/>
-<div class="nav">
-  <div class="back" @click="backToClient"></div>
-  <div class="carcass">
-    <p>
-      Каркас
-    </p>
+  <Header  :client="id" :clientData="true"/>
+  <div class="nav">
+    <div class="back" @click="backToClient"></div>
+    <div class="carcass">
+      <p>
+        Каркас
+      </p>
+    </div>
   </div>
-</div>
-<main>
-  <form action="">
-    <div class="adress">
-      <input type="text" placeholder="Введите адрес объекта строительства" :readonly="isReadOnly">
-      <input type="button" value="Сохранить" v-if="!isReadOnly">
-
-      <input type="reset" value="Очистить расчет">
-    </div>
-    <h3>Исходные данные</h3>
-    <div class="table floorsInput">Количество этажей <input type="number" placeholder="Введите число этажей" v-model="floorsCount" @change="duplicateFloors"> </div>
-    <div v-for="(currentFloor, index) in floors" :key="index">
-      <h3>Этаж: {{ index + 1 }}</h3>
-      <Floor :currentFloor="index"></Floor>
-    </div>
-
-
-
-    <input type="submit" value="Рассчитать">
-  </form>
-</main>
+  <main>
+    <form action="post" @submit.prevent="saveCalculation">
+      <div class="adress">
+        <input type="text" placeholder="Введите адрес объекта строительства" v-model="addres" :readonly="isReadOnly">
+        <input type="button" value="Сохранить" @click="saveAddress" v-if="!isReadOnly">
+        <input type="reset" value="Очистить расчет">
+      </div>
+      <h3>Исходные данные</h3>
+      <div class="table floorsInput">Количество этажей <input type="number" placeholder="Введите число этажей" v-model="floorsCount" @change="duplicateFloors"> </div>
+      <div v-for="(currentFloor, index) in floors" :key="index">
+        <h3>Этаж: {{ index + 1 }}</h3>
+        <Floor :currentFloor="index"></Floor>
+      </div>
+      <input type="submit" value="Рассчитать"  @click="saveCalculation">
+    </form>
+  </main>
 </template>
 
 <script>
+import {getClient, createCalculation} from "@/api.js";
+
 export default {
   components: {
     Floor
   },
   props: {
     id: String,
-    createMode: String
+    createMode: String,
+    numbers: String,
+    calculationData: {}
   },
   data() {
     return {
       floorsCount: 1, // Начальное количество этажей
       floors: [{}], // Массив с данными для каждого этажа, начинаем с одного пустого объекта
-      isReadOnly: false
+      isReadOnly: false,
+      addres: "",
+      calculation: {customerId: {id: parseInt(this.id) }, addressObjectConstractions: "", number: this.numbers, createdDate: new Date(), calculationStateId: {id: 1}}
     };
   },
   mounted() {
@@ -85,6 +87,36 @@ export default {
     backToClient() {
       this.$router.push({ name: "clientPage", props: { id: this.id } });
     },
+    saveAddress() {
+      if (this.addres === "") {
+        console.log("нет")
+      }
+      else {
+        this.calculation.addressObjectConstractions = this.addres;
+        this.isReadOnly = !this.isReadOnly;
+        console.log(this.calculation);
+      }
+    },
+    saveCalculation() {
+      // if (this.addres === "") {
+      //   console.log("нет")
+      // }
+      // else {
+      //   createCalculation(this.calculation)
+      //       .then(data => {
+      //         this.calculation = data;
+      //         this.$router.push({ path: "/calculation/" + this.calculation.id + "/" + this.id});
+      //       })
+      //       .catch(error => {
+      //         console.error("Произошла ошибка: ", error);
+      //       });
+      //   console.log(this.calculation);
+      // }
+      console.log("FLOORS")
+      for (const fl in this.floors) {
+        console.log(fl);
+      }
+    }
   }
 }
 </script>
--- src/components/SignInPage.vue
+++ src/components/SignInPage.vue
@@ -4,28 +4,29 @@ import LogoSignIn from "@/components/icons/LogoSignIn.vue";
 </script>
 
 <template>
-<div class="auth-container">
-  <div class="logo">
-    <LogoSignIn />
+  <div class="auth-container">
+    <div class="logo">
+      <LogoSignIn />
+    </div>
+    <!-- Форма авторизации -->
+    <div class="form">
+      <form @submit.prevent="login">
+        <input type="text" placeholder="Введите логин" class="login" id="userlogin" v-model="userlogin">
+        <input type="password" placeholder="Введите пароль" class="pass" id="userpassword" v-model="userpassword">
+        <button type="submit" class="enter">Войти</button>
+      </form>
+    </div>
   </div>
-  <!-- Форма авторизации -->
-  <div class="form">
-    <form @submit.prevent="login">
-      <input type="text" placeholder="Введите логин" class="login" id="userlogin" v-model="userlogin">
-      <input type="password" placeholder="Введите пароль" class="pass" id="userpassword" v-model="userpassword">
-      <button type="submit" class="enter">Войти</button>
-    </form>
+  <!-- Крестик закрытия -->
+  <div>
+    <span class="close-button" @click="goBack">&times;</span>
   </div>
-</div>
-<!-- Крестик закрытия -->
-<div>
-  <span class="close-button" @click="goBack">&times;</span>
-</div>
 </template>
 
 <script>
 import axios from "axios";
-import router from "@/components/router.js";
+import router from "@/router.js";
+import {signIn} from "@/api.js";
 
 export default {
   data() {
@@ -44,34 +45,7 @@ export default {
         username: this.userlogin,
         password: this.userpassword
       };
-      const response = await axios.post("http://localhost:8080/auth/sign-in", auth);
-
-      if (response.data) {
-        this.$store.commit('setToken', response.data.token);
-        this.$store.dispatch('loadToken');
-        const token = this.$store.state.token
-        const config = {
-          headers: { Authorization: `Bearer ${token}` }
-        };
-        const responseUser = await axios.get('http://localhost:8080/api/users/get', config);
-
-        if (responseUser.data) {
-          let user = responseUser.data;
-              console.log("USER: " + user);
-              this.$store.commit('setUser', user);
-              console.log(response.data.token);
-              this.$router.push({name: "clientsPage"});
-
-              // Обработка данных о пользователе
-        } else {
-          console.log(response.data.message);
-          console.log("clear token");
-          this.$store.commit('clearToken');
-          // Обработка ошибки запроса
-        }
-      } else {
-        console.log(response.data.message);
-      }
+      signIn(this.userlogin, this.userpassword);
     },
   },
   mounted() {
@@ -89,9 +63,9 @@ export default {
 </style>
 
 <style>
-  @import url(http://fonts.googleapis.com/css?family=Open+Sans:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i);
+@import url(http://fonts.googleapis.com/css?family=Open+Sans:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i);
 </style>
 
 <style>
-  @import url(http://fonts.googleapis.com/css?family=Open+Sans:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i);
+@import url(http://fonts.googleapis.com/css?family=Open+Sans:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i);
 </style>
